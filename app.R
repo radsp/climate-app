7808f4f0e7f07a09be43f70d3d5a034fa1731102
Radina Soebiyanto <rsoebiyanto@gmail.com>
2022-10-12 17:07:27 -0400

    Manually install scales package

--- app.R
+++ app.R
@@ -13,10 +13,11 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(plyr, tidyverse, ggplot2, leaflet, plotly, 
                shiny, shinyjs, shinythemes, shinyWidgets, shinyBS, shinybusy,
                shinydashboard, shinycssloaders, scales)
-
+install.packages("scales")
 
 library(civis)
 library(bslib)
+library(scales)
 
 # Source ------------------------------------------------------------------------
 
